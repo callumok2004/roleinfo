866ca01e6fbc7461a8a38a8f733d5b490707f9e3
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2022-01-15 21:10:54 +0000

    Update sv_roleinfo.lua

--- lua/autorun/server/sv_roleinfo.lua
+++ lua/autorun/server/sv_roleinfo.lua
@@ -1,9 +1,9 @@
 util.AddNetworkString("CustomRole_Helper_OpenWindow")
 
 hook.Add("PlayerSay", "CustomRolesHelper_Say", function(ply, chattext)
-	if (string.sub(string.lower(chattext), 1, #"!roleinfo") == string.lower("!roleinfo")) then
+	if (string.sub(string.lower(chattext), 1, #"!roleinfo") == "!roleinfo") then
 		net.Start("CustomRole_Helper_OpenWindow")
-		net.WriteString(ply:GetRoleString(), 32)
+		net.WriteString(ply:GetRoleString())
 		net.Send(ply)
 		return ""
 	end
